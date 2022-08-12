2ca2b2b236e03c2537c1c6d9bf47c462bae71144
EXXXI <ttta4@tvoyama.ne.jp>
2022-08-12 12:43:20 +0900

    錬成防具でスキル5種がありえるようなので入力欄を追加

--- src/RiseSim/ViewModels/SubViews/LicenseTabViewModel.cs
+++ src/RiseSim/ViewModels/SubViews/LicenseTabViewModel.cs
@@ -39,7 +39,7 @@ namespace RiseSim.ViewModels.SubViews
             // バージョン・ライセンス表示
             StringBuilder sb = new();
             sb.Append("■バージョン\n");
-            sb.Append("20220811.4\n");
+            sb.Append("20220811.5\n");
             sb.Append('\n');
             sb.Append("■このシミュのライセンス\n");
             sb.Append("GNU General Public License v3.0\n");
