f3242fb160cea93a7278a7d8e7f8074e2bfeb8f3
EXXXI <ttta4@tvoyama.ne.jp>
2022-08-14 21:35:31 +0900

    特定の環境で、泣きシミュ様でエクスポートしたお守りをインポートした場合に読み込めない不具合を修正

--- src/RiseSim/ViewModels/SubViews/LicenseTabViewModel.cs
+++ src/RiseSim/ViewModels/SubViews/LicenseTabViewModel.cs
@@ -39,7 +39,7 @@ namespace RiseSim.ViewModels.SubViews
             // バージョン・ライセンス表示
             StringBuilder sb = new();
             sb.Append("■バージョン\n");
-            sb.Append("20220814.0\n");
+            sb.Append("20220814.1\n");
             sb.Append('\n');
             sb.Append("■このシミュのライセンス\n");
             sb.Append("GNU General Public License v3.0\n");
