0e9b24925b5f82d38b292d0fa1d44d8ee03f4d63
xztaityozx <taityonohibi@gmail.com>
2022-08-21 12:09:57 +0900

    選択時にオレンジ色の枠を表示するようにした

--- src/RiseSim/ViewModels/Controls/SkillPickerSelectorViewModel.cs
+++ src/RiseSim/ViewModels/Controls/SkillPickerSelectorViewModel.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Drawing;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Media;
 using Prism.Mvvm;
 using Reactive.Bindings;
 using SimModel.Model;
@@ -23,25 +21,27 @@ namespace RiseSim.ViewModels.Controls
         /// </summary>
         public ReactivePropertySlim<Skill> SelectedSkill { get; set; }
 
-        /// <summary>
-        /// ComboBoxの背景色
-        /// </summary>
-        public Color BackgroundColor { get; set; } = Color.White;
+        private bool skillSelected = false;
+
+        public bool Selected
+        {
+            get => skillSelected;
+            set => SetProperty(ref skillSelected, value);
+        }
 
         public SkillPickerSelectorViewModel(Skill skill)
         {
             Items = new ReactivePropertySlim<ObservableCollection<Skill>>
             {
                 Value = new ObservableCollection<Skill>(
-                    Enumerable.Range(0, skill.Level).Select(level => skill with { Level = level })
+                    Enumerable.Range(0, skill.Level + 1).Select(level => skill with { Level = level })
                 )
             };
 
             SelectedSkill = new ReactivePropertySlim<Skill> { Value = Items.Value.First() };
             SelectedSkill.Subscribe(selected =>
             {
-                // Lvが0以外(=選択したとき)は背景色を黄色にする
-                BackgroundColor = selected.Level == 0 ? Color.White : Color.Yellow;
+                Selected = selected.Level != 0;
             });
         }
     }
