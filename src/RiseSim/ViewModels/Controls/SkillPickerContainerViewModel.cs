7e0ff33e3771c266cd2494f556c60d052bf13302
xztaityozx <taityonohibi@gmail.com>
2022-08-21 12:16:31 +0900

    SkillPickerSelectorを内包する折りたためるコンテナを追加

---
+++ src/RiseSim/ViewModels/Controls/SkillPickerContainerViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Prism.Mvvm;
+using Reactive.Bindings;
+using SimModel.Model;
+
+namespace RiseSim.ViewModels.Controls
+{
+    internal class SkillPickerContainerViewModel : BindableBase
+    {
+        /// <summary>
+        /// Expanderに設定するタイトル。スキルカテゴリを設定する想定
+        /// </summary>
+        public string Header { get; init; }
+        public ReactivePropertySlim<ObservableCollection<SkillPickerSelectorViewModel>> SkillPickerSelectors { get; init; }
+
+        public SkillPickerContainerViewModel(string categoryName, IEnumerable<Skill> skills)
+        {
+            Header = categoryName;
+            SkillPickerSelectors = new ReactivePropertySlim<ObservableCollection<SkillPickerSelectorViewModel>>
+            {
+                Value = new ObservableCollection<SkillPickerSelectorViewModel>(skills.Select(x =>
+                    new SkillPickerSelectorViewModel(x)))
+            };
+        }
+    }
+}
