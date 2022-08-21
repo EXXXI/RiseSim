bacdaf96e4c1e3d3bf3481ec35aa228266ad2cb8
xztaityozx <taityonohibi@gmail.com>
2022-08-21 17:11:44 +0900

    不要なusingを削除

--- src/RiseSim/App.xaml.cs
+++ src/RiseSim/App.xaml.cs
@@ -16,12 +16,6 @@
  */
 using RiseSim.ViewModels;
 using RiseSim.Views;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace RiseSim
